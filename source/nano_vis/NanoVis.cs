35e8ccdc6c08b2941f1cf8d770ddf16d8b554fa6
demiurghg <demiurghg@6d03c5d0-0fef-11df-8179-3f26cfb4ba5f>
2010-02-02 11:52:03 +0000

    Initial commit

---
+++ source/nano_vis/NanoVis.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Diagnostics;
+using SampleFramework;
+using SlimDX;
+using SlimDX.Direct3D9;
+using OpenBabel;
+
+namespace nano_vis
+{
+	enum  OpenBabelDataType	{
+	  UndefinedData = 0, PairData = 1, EnergyData = 2, CommentData = 3,
+	  ConformerData = 4, ExternalBondData = 5, RotamerList = 6, VirtualBondData = 7,
+	  RingData = 8, TorsionData = 9, AngleData = 10, SerialNums = 11,
+	  UnitCell = 12, SpinData = 13, ChargeData = 14, SymmetryData = 15,
+	  ChiralData = 16, OccupationData = 17, DensityData = 18, ElectronicData = 19,
+	  VibrationData = 20, RotationData = 21, NuclearData = 22, SetData = 23,
+	  GridData = 24, VectorData = 25, MatrixData = 26, CustomData0 = 16384,
+	  CustomData1 = 16385, CustomData2 = 16386, CustomData3 = 16387, CustomData4 = 16388,
+	  CustomData5 = 16389, CustomData6 = 16390, CustomData7 = 16391, CustomData8 = 16392,
+	  CustomData9 = 16393, CustomData10 = 16394, CustomData11 = 16395, CustomData12 = 16396,
+	  CustomData13 = 16397, CustomData14 = 16398, CustomData15 = 16399
+	}
+
+	class NanoVis : Game
+	{
+		public VisAtom	vis_atom	;
+		public VisCube	vis_cube;
+		
+		const int InitialWidth = 800;
+		const int InitialHeight = 600;
+		const int ParticleCount = 1500;
+		
+		float	cam_aspect		=	1.0f;
+		
+		float	view_distance	=	30;
+		Matrix	view_rotation	=	new Matrix();
+		Matrix	view_offset		=	new Matrix();
+		int		old_mouse_x;
+		int		old_mouse_y;
+
+		Camera	camera		= new Camera();
+		
+		Matrix	global_view;
+		Matrix	global_proj;
+		
+		SlimDX.Direct3D9.Font	font;
+		SlimDX.Direct3D9.Font	font_big;
+		
+		
+		Form	form_settings;
+		PropertyGrid	property_editor;
+		
+		//Molecule	molecule;
+
+		public Device Device
+		{
+			get { return GraphicsDeviceManager.Direct3D9.Device; }
+		}
+
+		public Color ClearColor
+		{
+			get;
+			set;
+		}
+
+		/*---------------------------------------------------------------------
+		 * Init :
+		---------------------------------------------------------------------*/
+		public NanoVis()
+		{
+			Debug.WriteLine("INIT : NanoVis");
+			ClearColor = Color.Black;
+
+			view_rotation			=	Matrix.Identity;
+
+			Window.ClientSize	=	new Size(InitialWidth, InitialHeight);
+			Window.Text			=	"Nano Visualizer";
+			Window.KeyDown		+=	Window_KeyDown;
+			Window.MouseMove	+=	Window_MouseMove;
+			Window.Width		=	800;
+			Window.Height		=	600;
+			Window.Top			=	100;
+			Window.Left			=	100;
+
+			camera.FieldOfView	=	(float)(Math.PI / 4);
+			camera.NearPlane	=	1.0f;
+			camera.FarPlane		=	1000.0f;
+			camera.Location		=	new Vector3(0, 0.0f, 0.0f);
+			camera.Target		=	Vector3.Zero;
+			camera.Up			=	new Vector3(0,0,1);
+			cam_aspect			=	(float)Window.Width / Window.Height;
+			
+			//	
+			//	Setup device :
+			//	
+			DeviceSettings	dev		= new DeviceSettings();
+            dev.DeviceVersion		= DeviceVersion.Direct3D9;
+            dev.Windowed			= true;
+            dev.BackBufferWidth		= InitialWidth;
+            dev.BackBufferHeight	= InitialHeight;
+            dev.MultisampleType		= MultisampleType.FourSamples;
+			
+			
+			vis_cube	=	new VisCube("test3.cube");
+			
+			GraphicsDeviceManager.ChangeDevice(dev);
+			//GraphicsDeviceManager.ChangeDevice(DeviceVersion.Direct3D9, true, InitialWidth, InitialHeight);
+			InitSettingsWindow();
+			
+		}
+		
+		void InitSettingsWindow()
+		{
+			form_settings			=	new Form();
+			form_settings.Text		=	"Settings";
+			form_settings.Width		=	300;
+			form_settings.Height	=	400;
+			
+			property_editor	=	new PropertyGrid();
+			property_editor.Parent	=	form_settings;
+			property_editor.Dock	=	DockStyle.Fill;
+			
+			property_editor.SelectedObject	=	vis_cube;
+			
+		}
+
+		void Window_KeyDown(object sender, KeyEventArgs e)
+		{
+			// F1 toggles between full screen and windowed mode
+			// Escape quits the application
+			if (e.KeyCode == Keys.F1) {
+				GraphicsDeviceManager.ToggleFullScreen();
+			}
+			if (e.KeyCode == Keys.F2) {
+				if (form_settings.IsDisposed) {
+					InitSettingsWindow();
+				} 
+				if (!form_settings.Visible) {
+					form_settings.Show(Window);
+				}
+			}
+			else if (e.KeyCode == Keys.Escape) {
+				Exit();
+			}
+		}
+		
+		void Window_MouseMove(object sender, MouseEventArgs e) 
+		{
+			int dx = e.X - old_mouse_x;
+			int dy = e.Y - old_mouse_y;
+			
+			float fx = -1 + 2 * (e.X / (float)Window.ClientSize.Width);
+			float fy =  1 - 2 * (e.Y / (float)Window.ClientSize.Height);
+
+			Vector4	pp0		= new Vector4(fx, fy, 0, 1);
+			Vector4	pp1		= new Vector4(fx, fy, 1, 1);
+			Matrix	unproj	= global_view * global_proj;
+			unproj.Invert();
+			Vector4	p0		= Vector4.Transform(pp0, unproj);
+			Vector4	p1		= Vector4.Transform(pp1, unproj);
+			p0 /= p0.W;
+			p1 /= p1.W;
+			
+			vis_cube.UpdateTraceRay(new Vector3(p0.X, p0.Y, p0.Z), new Vector3(p1.X, p1.Y, p1.Z));
+		
+			if (e.Button==MouseButtons.Left) {
+				view_rotation	*=	Matrix.RotationY(0.01f * dx);
+				view_rotation	*=	Matrix.RotationX(0.01f * dy);
+				//angle_z	+= 0.01f*dx;
+				//angle_y	+= 0.01f*dy;
+			}
+			if (e.Button==MouseButtons.Right) {
+				view_distance *= (float)Math.Pow(1.01f, (float)dy);
+			}
+		
+			old_mouse_x	=	e.X;
+			old_mouse_y	=	e.Y;
+		}
+		
+		public void DrawText ( int x, int y, string text )
+		{
+			font.DrawString(null, text, x,y, new Color4(1,1,1,1));
+		}
+
+		public void DrawTextBig ( int x, int y, string text )
+		{
+			font_big.DrawString(null, text, x,y, new Color4(1,1,1,1));
+		}
+
+		/*---------------------------------------------------------------------
+		 * Loading :
+		---------------------------------------------------------------------*/
+		protected override void LoadContent()
+		{
+			Debug.WriteLine("Loading content...");
+			cam_aspect	=	(float)Window.Width / Window.Height;
+			vis_atom	=	new VisAtom(Device);
+			font		=	new SlimDX.Direct3D9.Font(Device, new System.Drawing.Font(FontFamily.GenericSansSerif, 10));
+			font_big	=	new SlimDX.Direct3D9.Font(Device, new System.Drawing.Font(FontFamily.GenericSansSerif, 20));
+			
+			vis_cube.ReloadData( this );
+		}
+
+		/*---------------------------------------------------------------------
+		 * Unoading :
+		---------------------------------------------------------------------*/
+		protected override void UnloadContent()
+		{
+			Debug.WriteLine("Unloading content...");
+			
+			vis_atom.Dispose();
+			font.Dispose();
+		}
+
+		/*---------------------------------------------------------------------
+		 * Rendering :
+		---------------------------------------------------------------------*/
+		protected override void Draw(GameTime gameTime)
+		{
+			Matrix	view	=	Matrix.Identity;
+					view	*=	view_offset;
+					view	*=	view_rotation;
+					view	*=	Matrix.Translation(0, 0, -view_distance);
+			Matrix	proj	=	Matrix.PerspectiveFovRH(3.14f/6.0f, cam_aspect, 0.1f, 1000.0f);
+			
+			ClearColor	=	vis_cube.Background;
+			
+			try {
+		
+				Device.Clear(ClearFlags.Target | ClearFlags.ZBuffer, ClearColor, 1.0f, 0);
+				Device.BeginScene();
+
+					DrawParticles(view, proj);
+
+					vis_cube.Draw2D(this);
+
+				Device.EndScene();
+				
+			
+			} catch (Exception ex) {
+				Debug.WriteLine("**** EXCEPTION ****");
+				Debug.WriteLine(ex.Message);
+			}
+		}
+
+		void DrawParticles(Matrix matrix_view, Matrix matrix_proj)
+		{
+			try {
+				global_view				=	matrix_view;
+				global_proj				=	matrix_proj;
+				
+				vis_atom.matrix_proj	=	matrix_proj;
+				vis_atom.matrix_view	=	matrix_view;
+				vis_atom.light_dir		=	new Vector4(0,0,1,0);
+				vis_atom.view_dir		=	Vector4.Transform( new Vector4(0,0,1,0), Matrix.Invert(view_rotation));
+
+				Vector3 mol_center = new Vector3(0,0,0);		
+
+				view_offset	=	Matrix.Translation( -mol_center );
+				//vis_atom.DrawAxis( mol_center );
+				
+				vis_cube.Draw3D(this);
+				
+			} catch (Exception ex) {
+				Debug.WriteLine("ERROR : " + ex.Message);
+			}					
+		}
+	}
+}
---
+++ source/nano_vis/VisAtom.cs
@@ -0,0 +1,356 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using SampleFramework;
+using SlimDX;
+using SlimDX.Direct3D9;
+using OpenBabel;
+
+
+namespace nano_vis
+{
+	class VisAtom : IDisposable
+	{
+		Device	d3ddev;
+		Effect	atom_fx;
+		Effect	wire_fx;
+		Effect	vol_fx;
+		
+		public	Matrix	matrix_view;
+		public	Matrix	matrix_proj;
+		public	Vector4	light_dir;
+		public  Vector4 view_dir;
+		
+		Mesh	stick;
+		Mesh	ball;
+		VolumeTexture	volume_data = null;
+		OBVector3	vx;
+		OBVector3	vy;
+		OBVector3	vz;
+	
+		/*---------------------------------------------------------------------
+		 * VisAtom
+		---------------------------------------------------------------------*/
+		public VisAtom(Device d3ddev)
+		{	
+			this.d3ddev	=	d3ddev;
+			string error = "";
+			try {
+				atom_fx =	Effect.FromFile(d3ddev, "atom.fx",		ShaderFlags.None);
+				wire_fx =	Effect.FromFile(d3ddev, "wire.fx",		ShaderFlags.None);
+				vol_fx	=	Effect.FromFile(d3ddev, "volume.fx",	ShaderFlags.None);
+				
+			} catch (Exception ex) {
+				Debug.WriteLine(ex.Message);
+				Debug.WriteLine(error);
+				throw;
+			}
+			
+			Mesh stick	=	Mesh.CreateCylinder(d3ddev, 1, 1, 1, 32, 1);
+			Mesh ball	=	Mesh.CreateSphere(d3ddev, 1, 32, 16);
+
+            VertexElement[] decl = new VertexElement[] {
+                new VertexElement(0, 0,  DeclarationType.Float3, DeclarationMethod.Default, DeclarationUsage.Position, 0),
+				new VertexElement(0, 12, DeclarationType.Float3, DeclarationMethod.Default, DeclarationUsage.Normal, 0),
+				new VertexElement(0, 24, DeclarationType.Float3, DeclarationMethod.Default, DeclarationUsage.TextureCoordinate, 0),
+				new VertexElement(0, 36, DeclarationType.Float4, DeclarationMethod.Default, DeclarationUsage.Color, 0),
+				VertexElement.VertexDeclarationEnd
+            };
+			
+			this.stick	= stick.Clone(d3ddev, 0, decl);
+			this.ball	= ball.Clone(d3ddev, 0, decl);
+			
+			stick.Dispose();
+			ball.Dispose();
+			
+			//for (int i=0; i<decl.Length; i++) {
+			//    Debug.WriteLine( decl[i].ToString() );
+			//}
+		}
+		
+		public void Dispose()
+		{
+			if (atom_fx	!=	null	)	{	atom_fx.Dispose();		}
+			if (wire_fx	!=	null	)	{	wire_fx.Dispose();		}
+			if (vol_fx	!=	null	)	{	vol_fx.Dispose();		}
+			if (stick	!=	null	)	{	stick.Dispose();		}
+			if (ball	!=	null	)	{	ball.Dispose();			}
+			if (volume_data!=null	)	{	volume_data.Dispose();	}			
+		}
+
+		/*---------------------------------------------------------------------
+		 * 
+		---------------------------------------------------------------------*/
+		public void UpdateVolume( OBGridData grid )
+		{
+			int nx=100, ny=100, nz=100;
+			
+			vx	=	grid.GetXAxis();
+			vy	=	grid.GetYAxis();
+			vz	=	grid.GetZAxis();
+			
+			Debug.WriteLine(vx.x().ToString());
+			Debug.WriteLine(vx.y().ToString());
+			Debug.WriteLine(vx.z().ToString());
+			Debug.WriteLine(vy.x().ToString());
+			Debug.WriteLine(vy.y().ToString());
+			Debug.WriteLine(vy.z().ToString());
+			Debug.WriteLine(vz.x().ToString());
+			Debug.WriteLine(vz.y().ToString());
+			Debug.WriteLine(vz.z().ToString());
+			
+			//volume_data	=	new VolumeTexture(d3ddev, nx, ny, nz, 1, Usage.None, Format.L8, Pool.Managed);
+			if (volume_data!=null) {
+				volume_data.Dispose();
+			}
+			volume_data	=	new VolumeTexture(d3ddev, nx, ny, nz, 1, Usage.None, Format.R32F, Pool.Managed);
+			
+			DataBox	box = volume_data.LockBox(0, LockFlags.None);
+
+			for (int i=0; i<nx; i++) {
+				for (int j=0; j<ny; j++) {
+					for (int k=0; k<nz; k++) {
+					
+						int pos	=	4*i + j * box.RowPitch + k * box.SlicePitch;
+						box.Data.Seek(pos, System.IO.SeekOrigin.Begin);
+					
+						box.Data.Write<float>( (float) grid.GetValue(i,j,k)  );
+					}
+				}
+			}		
+			
+				
+
+			volume_data.UnlockBox(0);
+		}
+		
+		/*---------------------------------------------------------------------
+		 * Setup stuff :
+		---------------------------------------------------------------------*/
+
+		Matrix InvTranspose(Matrix m) {
+			Matrix w = Matrix.Transpose( Matrix.Invert( m ) );
+			w.M14 = 0;
+			w.M24 = 0;
+			w.M34 = 0;
+			w.M44 = 0;
+			w.M41 = 0;
+			w.M42 = 0;
+			w.M43 = 0;
+			w.M44 = 0;
+			return w;
+		}
+		
+		public void SetupSolid ( Matrix w, Matrix v, Matrix p, Vector4 lightdir, Vector4 viewdir, Vector4 color )
+		{
+			atom_fx.Technique = "solid_body";
+			atom_fx.SetValue("matrix_world",	w );
+			atom_fx.SetValue("matrix_view",		v );
+			atom_fx.SetValue("matrix_proj",		p );
+			atom_fx.SetValue("matrix_w_it",		InvTranspose( w ) );
+
+			atom_fx.SetValue("matrix_wv_it",	InvTranspose( w * v ) );
+			atom_fx.SetValue("light_dir",		Vector4.Normalize(lightdir));
+			atom_fx.SetValue("view_dir",		Vector4.Normalize(viewdir));
+			atom_fx.SetValue("atom_color",		color);
+		}		
+
+		public void SetupAxis ( Matrix w, Matrix v, Matrix p )
+		{
+			wire_fx.Technique = "axis";
+			wire_fx.SetValue("matrix_world",	w );
+			wire_fx.SetValue("matrix_view",		v );
+			wire_fx.SetValue("matrix_proj",		p );
+		}
+
+		public void SetupVolume ( Matrix w, Matrix v, Matrix p )
+		{
+			vol_fx.Technique = "volume";
+			vol_fx.SetValue("matrix_world",		w );
+			vol_fx.SetValue("matrix_view",		v );
+			vol_fx.SetValue("matrix_proj",		p );
+			
+			vol_fx.SetTexture("volume_data_tex", volume_data);
+		}
+
+		/*---------------------------------------------------------------------
+		 * Drawing stuff :
+		---------------------------------------------------------------------*/
+
+		protected struct Vert {
+			public Vert(Vector3 _p, uint _c) {
+				p = _p;
+				c = _c;
+			}
+			Vector3 p;
+			uint	c;
+		};
+		
+		protected struct VertUV {
+			public VertUV(Vector3 _p, uint _c, Vector2 _uv, Vector2 _uv1) {
+				p	= _p;
+				c	= _c;
+				uv	= _uv;
+				uv1	= _uv1;
+			}
+			Vector3 p;
+			uint	c;
+			Vector2 uv;
+			Vector2 uv1;
+		};
+		
+		public void DrawAxis(Vector3 pos)
+		{
+			SetupAxis(Matrix.Identity, matrix_view, matrix_proj);
+
+		    wire_fx.Begin();
+		    wire_fx.BeginPass(0);
+		    
+				Vert[] verts = { 
+					new Vert(new Vector3(0,0,0)+pos, 0xFFFF0000),  new Vert(new Vector3(1,0,0)+pos, 0xFFFF0000), 
+					new Vert(new Vector3(0,0,0)+pos, 0xFF00FF00),  new Vert(new Vector3(0,1,0)+pos, 0xFF00FF00), 
+					new Vert(new Vector3(0,0,0)+pos, 0xFF0000FF),  new Vert(new Vector3(0,0,1)+pos, 0xFF0000FF), 
+				};
+				
+				d3ddev.VertexFormat	=	VertexFormat.Position | VertexFormat.Diffuse;
+				d3ddev.DrawUserPrimitives<Vert>(PrimitiveType.LineList, 3, verts);
+
+		    wire_fx.EndPass();
+		    wire_fx.End();
+		}
+		
+		
+		public void DrawVolume(float slice)
+		{
+			SetupVolume(Matrix.Identity, matrix_view, matrix_proj);
+			
+			vol_fx.Begin();
+			vol_fx.BeginPass(0);
+			
+//			float BA = 0.529177249f;
+			float BA = 50.0f;
+			
+			float z	=	slice * (float)vz.z() * BA;
+			float w =   slice / 2.0f + 0.5f;
+			
+				VertUV[]	verts = {
+					new VertUV(new Vector3(+1*(float)vx.x()*BA, +1*(float)vy.y()*BA, z), 0xFF00FF00, new Vector2(1,1), new Vector2(w,0)), 
+					new VertUV(new Vector3(-1*(float)vx.x()*BA, +1*(float)vy.y()*BA, z), 0xFF000000, new Vector2(0,1), new Vector2(w,0)), 
+					new VertUV(new Vector3(-1*(float)vx.x()*BA, -1*(float)vy.y()*BA, z), 0x00000000, new Vector2(0,0), new Vector2(w,0)), 
+					new VertUV(new Vector3(+1*(float)vx.x()*BA, -1*(float)vy.y()*BA, z), 0x0000FF00, new Vector2(1,0), new Vector2(w,0)),
+				};
+			
+				d3ddev.VertexFormat	=	VertexFormat.Position | VertexFormat.Diffuse | VertexFormat.Texture2;
+				d3ddev.DrawUserPrimitives<VertUV>(PrimitiveType.TriangleFan, 2, verts);
+			
+			vol_fx.EndPass();
+			vol_fx.End();
+		}
+		
+		
+		public void DrawCage(Vector3 center, Vector3 xyz_size, float tail_size, Vector4 color) 
+		{
+			SetupAxis(Matrix.Identity, matrix_view, matrix_proj);
+
+		    wire_fx.Begin();
+		    wire_fx.BeginPass(0);
+		    
+		    uint icolor	= 0xFFFFFFFF;
+		    float x		= 0.5f*xyz_size.X;
+		    float y		= 0.5f*xyz_size.Y;
+		    float z		= 0.5f*xyz_size.Z;
+		    float s		= tail_size;
+		    Vector3	p	= center;
+		    
+				Vert[] verts = { 
+					new Vert(new Vector3( x, y, z) + p, icolor),  new Vert(new Vector3( x-s, y-0, z-0) + p, icolor), 
+					new Vert(new Vector3( x, y, z) + p, icolor),  new Vert(new Vector3( x-0, y-s, z-0) + p, icolor), 
+					new Vert(new Vector3( x, y, z) + p, icolor),  new Vert(new Vector3( x-0, y-0, z-s) + p, icolor), 
+
+					new Vert(new Vector3(-x, y, z) + p, icolor),  new Vert(new Vector3(-x+s, y-0, z-0) + p, icolor), 
+					new Vert(new Vector3(-x, y, z) + p, icolor),  new Vert(new Vector3(-x+0, y-s, z-0) + p, icolor), 
+					new Vert(new Vector3(-x, y, z) + p, icolor),  new Vert(new Vector3(-x+0, y-0, z-s) + p, icolor), 
+
+
+					new Vert(new Vector3( x,-y, z) + p, icolor),  new Vert(new Vector3( x-s,-y+0, z-0) + p, icolor), 
+					new Vert(new Vector3( x,-y, z) + p, icolor),  new Vert(new Vector3( x-0,-y+s, z-0) + p, icolor), 
+					new Vert(new Vector3( x,-y, z) + p, icolor),  new Vert(new Vector3( x-0,-y+0, z-s) + p, icolor), 
+
+					new Vert(new Vector3(-x,-y, z) + p, icolor),  new Vert(new Vector3(-x+s,-y+0, z-0) + p, icolor), 
+					new Vert(new Vector3(-x,-y, z) + p, icolor),  new Vert(new Vector3(-x+0,-y+s, z-0) + p, icolor), 
+					new Vert(new Vector3(-x,-y, z) + p, icolor),  new Vert(new Vector3(-x+0,-y+0, z-s) + p, icolor), 
+
+
+
+					new Vert(new Vector3( x, y,-z) + p, icolor),  new Vert(new Vector3( x-s, y-0,-z+0) + p, icolor), 
+					new Vert(new Vector3( x, y,-z) + p, icolor),  new Vert(new Vector3( x-0, y-s,-z+0) + p, icolor), 
+					new Vert(new Vector3( x, y,-z) + p, icolor),  new Vert(new Vector3( x-0, y-0,-z+s) + p, icolor), 
+
+					new Vert(new Vector3(-x, y,-z) + p, icolor),  new Vert(new Vector3(-x+s, y-0,-z+0) + p, icolor), 
+					new Vert(new Vector3(-x, y,-z) + p, icolor),  new Vert(new Vector3(-x+0, y-s,-z+0) + p, icolor), 
+					new Vert(new Vector3(-x, y,-z) + p, icolor),  new Vert(new Vector3(-x+0, y-0,-z+s) + p, icolor), 
+
+
+					new Vert(new Vector3( x,-y,-z) + p, icolor),  new Vert(new Vector3( x-s,-y+0,-z+0) + p, icolor), 
+					new Vert(new Vector3( x,-y,-z) + p, icolor),  new Vert(new Vector3( x-0,-y+s,-z+0) + p, icolor), 
+					new Vert(new Vector3( x,-y,-z) + p, icolor),  new Vert(new Vector3( x-0,-y+0,-z+s) + p, icolor), 
+
+					new Vert(new Vector3(-x,-y,-z) + p, icolor),  new Vert(new Vector3(-x+s,-y+0,-z+0) + p, icolor), 
+					new Vert(new Vector3(-x,-y,-z) + p, icolor),  new Vert(new Vector3(-x+0,-y+s,-z+0) + p, icolor), 
+					new Vert(new Vector3(-x,-y,-z) + p, icolor),  new Vert(new Vector3(-x+0,-y+0,-z+s) + p, icolor), 
+				};
+				
+				d3ddev.VertexFormat	=	VertexFormat.Position | VertexFormat.Diffuse;
+				d3ddev.DrawUserPrimitives<Vert>(PrimitiveType.LineList, 24, verts);
+
+		    wire_fx.EndPass();
+		    wire_fx.End();
+		}
+
+		public void DrawStick(Vector3 position, Vector3 direction, float length, float radius, Vector4 color)
+		{
+			Vector3 dir		= Vector3.Normalize(direction);
+			Vector3 up		= new Vector3(0,0,1);
+			
+			if (Math.Abs(Vector3.Dot(dir, up))==1) {
+				up = new Vector3(1,0,0);
+			}
+			
+			Matrix	w	=	Matrix.Identity;
+					w	*=	Matrix.Scaling(radius, radius, length);
+					w	*=	Matrix.Translation(0,0, 0.5f * length);
+					w	*=	Matrix.Invert(Matrix.LookAtLH(position, position + dir, up));
+
+			SetupSolid( w, matrix_view, matrix_proj, light_dir, view_dir, color );
+
+		    atom_fx.Begin();
+		    atom_fx.BeginPass(0);
+
+				stick.DrawSubset(0);
+
+		    atom_fx.EndPass();
+		    atom_fx.End();
+		}
+		
+		
+		public void DrawBall(Vector3 position, float radius, Vector4 color)
+		{
+			Matrix	w	=	Matrix.Identity;
+					w	*=	Matrix.Scaling(radius, radius, radius);
+					w	*=	Matrix.Translation(position);
+
+			SetupSolid( w, matrix_view, matrix_proj, light_dir, view_dir, color );
+
+			atom_fx.Begin();
+			atom_fx.BeginPass(0);
+
+				ball.DrawSubset(0);
+
+			atom_fx.EndPass();
+			atom_fx.End();
+		}
+		
+	}
+}
