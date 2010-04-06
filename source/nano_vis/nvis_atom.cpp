f9bc2da9001e908e8cc5c5c3f579289db9d3dec0
demiurghg <demiurghg@6d03c5d0-0fef-11df-8179-3f26cfb4ba5f>
2010-04-06 09:30:20 +0000

    Dev: sticks and perspective

--- source/nano_vis/nvis_atom.cpp
+++ source/nano_vis/nvis_atom.cpp
@@ -140,15 +140,21 @@ void ENanoVis::RenderShot( lua_State *L )
 	//
 	//	read table :
 	//	
-	EName	path	=	"";
-	EName	shot	=	"shot.png";
-	float	yaw		=	0;
-	float	roll	=	0;
-	float	pitch	=	0;
-	LuaGetField( L, 1, "yaw",	yaw		);
-	LuaGetField( L, 1, "roll",	roll	);
-	LuaGetField( L, 1, "pitch",	pitch	);
-	LuaGetField( L, 1, "path",	path	);
+	EName	path		=	"";
+	EName	shot		=	"shot.png";
+	float	yaw			=	0;
+	float	roll		=	0;
+	float	pitch		=	0;
+	float	distance	=	10;
+	float	atom_scale	=	0.1f;
+	float	bond_radius	=	0.03f;
+	LuaGetField( L, 1, "yaw",			yaw			);
+	LuaGetField( L, 1, "roll",			roll		);
+	LuaGetField( L, 1, "pitch",			pitch		);
+	LuaGetField( L, 1, "path",			path		);
+	LuaGetField( L, 1, "distance",		distance	);
+	LuaGetField( L, 1, "atom_scale",	atom_scale	);
+	LuaGetField( L, 1, "bond_radius",	bond_radius	);
 
 	//
 	//	load data :
@@ -169,18 +175,23 @@ void ENanoVis::RenderShot( lua_State *L )
 	//	setup view :
 	//	
 	D3DXMATRIX	world;
-	D3DXMATRIX	view, view_i, r_yaw, r_pitch, r_roll;
+	D3DXMATRIX	view, view_i, r_yaw, r_pitch, r_roll, z_view_offset;
 	D3DXMATRIX	proj;
 
-	D3DXMatrixIdentity( &world );	
-	D3DXMatrixRotationZ( &r_yaw,		deg2rad(yaw) );
-	D3DXMatrixRotationY( &r_pitch,		deg2rad(pitch) );
-	D3DXMatrixRotationX( &r_roll,		deg2rad(roll) );
-	view	=	r_roll * r_pitch * r_yaw;
-	D3DXMatrixOrthoRH( &proj, 16, 12, -16, 16 );
+	//	setup view matrix :
+	D3DXMatrixIdentity		( &world );	
+	D3DXMatrixRotationZ		( &r_yaw,			deg2rad(yaw) );
+	D3DXMatrixRotationY		( &r_pitch,			deg2rad(pitch) );
+	D3DXMatrixRotationX		( &r_roll,			deg2rad(roll) );
+	D3DXMatrixTranslation	( &z_view_offset,	0,0, -distance );
+	view	=	r_roll * r_pitch * r_yaw * z_view_offset;
+
+	//	setup projection :	
+	D3DXMatrixPerspectiveRH( &proj, 0.100f, 0.075f, 0.075f, 1000 );
 	
 	D3DXMatrixInverse( &view_i, NULL, &view );
-	
+
+	//	set shader constants :	
 	HRCALL( atom_fx->SetMatrix("matrix_world",	&world) );
 	HRCALL( atom_fx->SetMatrix("matrix_view",	&view) );
 	HRCALL( atom_fx->SetMatrix("matrix_proj",	&proj) );
@@ -201,21 +212,62 @@ void ENanoVis::RenderShot( lua_State *L )
 	uint n;
 	HRCALL( atom_fx->Begin(&n, 0) );
 	
-	for (uint i=0; i<n; i++) {
-		atom_fx->BeginPass(i);
+	for (uint pass=0; pass<n; pass++) {
+		atom_fx->BeginPass(pass);
 		
 			for (int i=1; i<=g_mol->NumAtoms(); i++) {
 				OBAtom	*a	=	g_mol->GetAtom(i);
 				
 				double *xyz = a->GetCoordinate();
 				
-				D3DXMatrixTranslation( &world, xyz[0], xyz[1], xyz[2] );
+				float asz = atom_scale;
+
+				D3DXMATRIX	t, s;				
+				D3DXMatrixTranslation( &t, xyz[0], xyz[1], xyz[2] );
+				D3DXMatrixScaling( &s, asz, asz, asz );
+				world = s*t;
 				
 				HRCALL( atom_fx->SetMatrix("matrix_world",	&world) );
 				atom_fx->CommitChanges();
 				
 				mesh_ball->DrawSubset(0);
 			}
+			
+			
+			for (int i=0; i<g_mol->NumBonds(); i++) {
+				OBBond	*b	=	g_mol->GetBond(i);
+				
+				double *xyz0 = b->GetBeginAtom()->GetCoordinate();;
+				double *xyz1 = b->GetEndAtom()->GetCoordinate();;
+				
+				D3DXVECTOR3	p0(xyz0[0], xyz0[1], xyz0[2]);
+				D3DXVECTOR3	p1(xyz1[0], xyz1[1], xyz1[2]);
+				D3DXVECTOR3 d	=	p1 - p0;
+				D3DXVECTOR3 dn;
+				D3DXVec3Normalize(&dn, &d);
+				D3DXVECTOR3	up(0,0,1);
+				if (dn.z==1 || dn.x==-1) {
+					up = D3DXVECTOR3(1,0,0);
+				}
+				D3DXMATRIX	s, t, aim, aim_i;
+				
+				D3DXMatrixIdentity(&s);
+				D3DXMatrixIdentity(&t);
+				D3DXMatrixIdentity(&aim);
+				D3DXMatrixIdentity(&aim_i);
+				
+				D3DXMatrixScaling		( &s,		bond_radius, bond_radius, D3DXVec3Length( &d ) );
+				D3DXMatrixTranslation	( &t,		0, 0, 0.5 * D3DXVec3Length( &d ) );
+				D3DXMatrixLookAtLH		( &aim,		&p0,  &p1,  &up  );
+				D3DXMatrixInverse		( &aim_i,	NULL,  &aim );
+				
+				world = s * t * aim_i;
+				
+				HRCALL( atom_fx->SetMatrix("matrix_world",	&world) );
+				atom_fx->CommitChanges();
+				
+				mesh_stick->DrawSubset(0);
+			} 
 			
 		
 		atom_fx->EndPass();
