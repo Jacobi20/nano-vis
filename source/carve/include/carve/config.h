56a95213421063cc4ea371088f5967accf4b28ad
demiurghg <demiurghg@6d03c5d0-0fef-11df-8179-3f26cfb4ba5f>
2010-07-13 08:17:45 +0000

    Dev: stuff

---
+++ source/carve/include/carve/config.h
@@ -0,0 +1,16 @@
+/* include/carve/config.h.  Generated from config.h.in by configure.  */
+#pragma once
+
+/* Define if using gnu libstdc++. */
+#define HAVE_LIBSTDCPP_UNORDERED_COLLECTIONS 
+
+/* Define if using boost collections. */
+/* #undef HAVE_BOOST_UNORDERED_COLLECTIONS */
+
+/* Define if std::unordered_map and std::unordered_set are supported by your
+   compiler. */
+/* #undef HAVE_STD_UNORDERED_COLLECTIONS */
+
+/* Define if TR1 collections are supportted by your compiler. */
+/* #undef HAVE_TR1_UNORDERED_COLLECTIONS */
+
