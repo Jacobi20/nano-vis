18abfa472428cb792587fe6f2e17ad6f695ff422
demiurghg <demiurghg@6d03c5d0-0fef-11df-8179-3f26cfb4ba5f>
2010-08-11 15:18:45 +0000

    Dev: new ship sim

---
+++ ship_sim/source/ship.h
@@ -0,0 +1,106 @@
+/*
+    The MIT License
+
+    Copyright (c) 2010 IFMO/GameDev Studio
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+*/
+
+#pragma once
+
+/*-----------------------------------------------------------------------------
+	Naive ship simulator :
+-----------------------------------------------------------------------------*/
+
+class EShip : public IShip {
+	public:
+							EShip				( lua_State *L, int idx );
+							~EShip				( void );
+			
+		//	general :		
+		virtual EString		Name				( void ) const { return name; }
+		virtual void		Simulate			( float dtime, IPxWaving waving );
+		virtual void		Render				( ERendEnv_s *rend_env );
+
+		//	ship properties :
+		virtual void		GetPose				( EVec4 &position, EQuat &orient );
+		virtual void		SetPose				( EVec4 &position, EQuat &orient );
+		
+		virtual void		BuildVoxels			( const EString path, float cube_size );
+		
+		virtual void		SetVisMesh			( const EString path );
+		virtual void		SetHSFMesh			( const EString path );
+		virtual void		SetHDFMesh			( const EString path );
+		virtual void		SetResistance		( float cx ) { water_resistance_cx = cx; }
+		virtual void		MakeRigidBody		( const EString path, float mass );
+		virtual void		AddForce			( EVec3 force, EVec3 point, bool local_point );
+		virtual void		AddMomentum			( EVec3 momentum, bool local_momentum );
+		
+		virtual EVec3		GetHSFMomentum		( void ) { return total_hsf_momentum; }
+		virtual float		GetHSFForce			( void ) { return total_hsf_force; }
+		virtual float		GetRightArm			( void ) { return right_arm; }
+
+		virtual EVec3		GetCenterMass		( void );
+		virtual EVec3		GetInertiaMomentum	( EVec3 axis );
+
+	protected:
+		EPlane				GetShipXOZPlane		( void );
+	
+		EString				name;
+		
+		float				total_hsf_force;
+		float				right_arm;
+		EVec3				total_hsf_momentum;
+		EVec3				center_of_hsf_force;
+		
+		float				water_resistance_cx;
+	
+		IPxTriMesh			GetSubmergedMesh		( const EMatrix4 &world, const EPlane &plane );
+	
+		void				UpdateForces	( float dtime, IPxWaving waving );
+		void				UpdateHSF		( float dtime, IPxWaving waving );
+		void				UpdateHDF		( float dtime, IPxWaving waving );
+		
+		void				UpdateHSFBox	( float dtime, IPxWaving waving );
+		void				UpdateHSFVoxel	( float dtime, IPxWaving waving );
+		void				UpdateHSFSurface( float dtime, IPxWaving waving );
+
+		EPxVoxelGrid	voxel_grid;
+		
+		
+		float			ship_length;
+		float			ship_width;
+		float			ship_height;
+		float			ship_mass;
+		float			self_time;
+	
+		IPxTriMesh		mesh_vis;
+		IPxTriMesh		mesh_hsf;
+		IPxTriMesh		mesh_hdf;
+		IPxTriMesh		mesh_submerged_hsf;
+		IPxTriMesh		mesh_submerged_hdf;
+
+		NxActor			*ship_body;
+		
+		IPxVar			ship_show_voxels;
+		IPxVar			ship_show_hull;
+		IPxVar			ship_show_submerge;
+		IPxVar			ship_hsf_method;
+	};
+	
---
+++ ship_sim/source/ship_mesh.cpp
@@ -0,0 +1,104 @@
+/*
+    The MIT License
+
+    Copyright (c) 2010 IFMO/GameDev Studio
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+*/
+
+#include "sci_local.h"
+#include "ship.h"
+
+/*-----------------------------------------------------------------------------
+	Ship mesh stuff :
+-----------------------------------------------------------------------------*/
+
+//
+//	EShip::SetVisMesh
+//
+void EShip::SetVisMesh( const EString path )
+{
+	LOGF("view mesh : %s", path.CStr());
+	mesh_vis		=	ge()->LoadMeshFromFile( path.CStr() );
+	//d3d_mesh_vis	=	sci_vis->CreateMesh( mesh_vis );
+}
+
+
+//
+//	EShip::SetHSFMesh
+//
+void EShip::SetHSFMesh( const EString path )
+{
+	LOGF("HSF mesh  : %s", path.CStr());
+	mesh_hsf	=	ge()->LoadMeshFromFile( path.CStr() );
+	
+	EBBox	bbox	=	mesh_hsf->ComputeBBox();
+	ship_length		=	bbox.Size().x;
+	ship_width		=	bbox.Size().y;
+	ship_height		=	bbox.Size().z;
+}
+
+
+//
+//	EShip::SetHDFMesh
+//
+void EShip::SetHDFMesh( const EString path )
+{
+	LOGF("HDF mesh  : %s", path.CStr());
+	mesh_hdf	=	ge()->LoadMeshFromFile( path.CStr() );
+}
+
+
+//
+//	EShip::MakeRigidBody
+//
+void EShip::MakeRigidBody( const EString path, float mass )
+{
+	LOGF("RB mesh   : %s", path.CStr());
+	IPxTriMesh phys_mesh	=	ge()->LoadMeshFromFile( path.CStr() );
+	
+	if (ship_body) {
+		sci_vis->GetNxScene()->releaseActor( *ship_body );
+	}
+	
+	ship_body	=	sci_vis->CreatePhysMesh( phys_mesh, EVec4(0,0,0,1), EQuat(0,0,0,1), mass );
+	ship_mass	=	ship_body->getMass();
+}
+
+
+//
+//	EShip::BuildVoxels
+//	
+void EShip::BuildVoxels( const EString path, float cube_size )
+{
+	LOGF("building voxels for : %s", path.CStr());
+	LOGF("voxel size : %g", cube_size);
+	voxel_grid	=	new EVoxelGrid();
+	
+	IPxTriMesh	mesh	=	ge()->LoadMeshFromFile( path.CStr() );
+
+	try {	
+		voxel_grid->BuildGrid( mesh, EVec3(0,0,0), cube_size, cube_size, cube_size );
+	} catch (exception &e) {
+		LOG_WARNING("%s", e.what());
+	}
+	
+	LOGF("done : %d voxels", voxel_grid->GetVoxelNum());
+}
+
