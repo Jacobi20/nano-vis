b4a190b72907aaf125a7c67150a7ebb053288fd0
demiurghg <demiurghg@6d03c5d0-0fef-11df-8179-3f26cfb4ba5f>
2010-08-17 07:51:28 +0000

    Dev: stuff

---
+++ ship_sim/data/shaders/shader_lib.lua
@@ -0,0 +1,61 @@
+-------------------------------------------------------------------------------
+-- The MIT License
+--
+-- Copyright (c) 2010 IFMO/GameDev Studio
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in
+-- all copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+-- THE SOFTWARE.
+-------------------------------------------------------------------------------
+
+
+-------------------------------------------------------------------------------
+--	shader library
+-------------------------------------------------------------------------------
+
+function define_dsn_shader ( path )
+	local injection = [[
+		surface.diffuse 	= 	sample_color ( sampler0, input.uv0 ).rgb;
+		surface.specular	=	sample_color ( sampler1, input.uv0 ).r;
+		surface.roughness	=	sample_color ( sampler1, input.uv0 ).g;
+		surface.metalness	=	sample_color ( sampler1, input.uv0 ).b;
+		surface.normal		=	sample_normal( sampler2, input.uv0 ).xyz;
+	]];
+
+	fr.define_shader {
+		name			=	path;
+		texture_path0	=	path;
+		texture_path1	=	postfix(path, "_spec");
+		texture_path2	=	postfix(path, "_local");
+		effect_path		=	inject_surface_fx( injection );
+		is_solid		=	true;
+	}
+end
+
+
+function define_diffuse_shader ( path )
+	local injection = [[
+		surface.diffuse 	= 	sample_color ( sampler0, input.uv0 );
+	]];
+
+	fr.define_shader {
+		name			=	path;
+		texture_path0	=	path;
+		effect_path		=	inject_surface_fx( injection );
+		is_solid		=	true;
+	}
+end
