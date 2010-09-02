8abd01239ef9a7f3efc9fab314bd3c443deb4000
demiurghg <demiurghg@6d03c5d0-0fef-11df-8179-3f26cfb4ba5f>
2010-09-02 17:12:18 +0000

    Dev: exp01.lua

--- trunk/ship_sim/gamedata/exp01/exp01.lua
+++ trunk/ship_sim/gamedata/exp01/exp01.lua
@@ -12,32 +12,32 @@ local	gamma	=	gravity * 1000;		--	water weight density
 local function sqr(a) return a*a; end;
 
 -----------------------------------------------------------------------
---	SUBMARINE  --------------------------------------------------------
+--	SUBMARINE  --
 -----------------------------------------------------------------------
 --	attributes :
 -----------------------------------------------------------------------
 
-local function S()
-	return (T-R)*math.sqrt(T*(2*R-T)) + R*R*(math.asin((T-R)/R) + math.pi/2 )	--	(1.2)
+local function S(t,r)	--	(1.2) 
+	return (t-r)*math.sqrt(t*(2*r-t)) + r*r*(math.asin((t-r)/r) + math.pi/2 );	
 end
 
+local	T			=	8;		--	submarine's initial draft - equlibrium keel depth
 local	R			=	5;		--	submarine's radius, m
 local	L			=	100;    --	submarine's length, m
-local	T			=	7;		--	submarine's initial draft - equlibrium keel depth
-local	Zg			=	-2;		--	center of mass vertical offset
+local	Zg			=	2;		--	center of mass vertical offset
 local	Zp			=	T - R;	--	distance between center and waterline
-local 	B0			=	math.sqrt(R*R - Zp*Zp);		--	initial waterline width 	(1.1)
-local	D			=	S() * L * gamma;			--	weight of the submarine		(1.2)
+local 	B0			=	2*math.sqrt(R*R - Zp*Zp);	--	initial waterline width 	(1.1)
+local	D			=	S(T,R) * L * gamma;			--	weight of the submarine		(1.2)
 local	M			=	D/g;						--	mass of the submarine		
 local	Ixx			=	M/12 * (B0*B0 + 4*Zg*Zg);	--	ineria						(1.8)
 	
 local 	lambda33	=	0;		--	attached water mass
 local 	lambda44	=	0;		--	attached water inertia
-local 	mu33		=	0.8;	--	linear damping coeeficient
-local 	mu44		=	0.8;	--	angular damping coeeficient
+local 	mu33		=	-3000000;	--	linear damping coeeficient
+local 	mu44		=	-1000000;	--	angular damping coeeficient
 	
 --	derived properties :
-local 	zeta		=	0;	--	linear offset
+local 	zeta		=	1;	--	linear offset
 local 	zeta_d		=	0;	--	linear velocity
 local 	zeta_dd		=	0;	--	linear acceleration
 local 	theta		=	0;	--	angle (roll)
@@ -61,14 +61,24 @@ local function Md(theta_d)
 	return mu44 * theta_d;
 end
 
---	(1.10)	--
+print(D);
+
+--	(1.10)	-- 
 local function Fr(zeta)
+	if zeta>(2*R-T) then 
+		return  - ( gamma * math.pi * R*R * L - D );
+	end
+	
+	if zeta<-T then
+		return D;
+	end
+	
 	local A, B, C, D;
 	A 	= R*R * math.asin((zeta+Zp)/R);
 	B 	= R*R * math.asin(Zp/R);
 	C	= (zeta+Zp) * math.sqrt(R*R - sqr(Zp+zeta));
 	D	= Zp * math.sqrt(R*R - Zp*Zp);
-	return gamma * L * (A - B + C - D);
+	return - gamma * L * (A - B + C - D);
 end
 
 --	(1.12)	--	
@@ -83,12 +93,17 @@ local function update_forces(dt)
 end
 
 
-local function integrate_velocities(dt)
+local function integrate(dt)
+	zeta_d = zeta_d + zeta_dd * dt;
+	zeta   = zeta + zeta_d * dt;
+
+	local Ftotal = Fr(zeta) + Fd(zeta_d);
+	zeta_dd = Ftotal / M;
 	
 end
 
 -----------------------------------------------------------------------
---	waving
+--	WAVING  --
 -----------------------------------------------------------------------
 
 local  waving = {
@@ -114,19 +129,62 @@ local  waving = {
 
 
 -----------------------------------------------------------------------
---	waving
+--	EXPERIMENT  --
 -----------------------------------------------------------------------
+
+local function plot(F, x0, x1, step)
+	local f = io.open("exp01/plot.txt", "w");
+	for x=x0, x1, step do
+		local y = F(x);
+		local out	= string.format("%g %g", x, y);
+		f:write(out.."\n");
+	end
+	io.close(f);
+end
+
+plot(Fr, -20, 7, 0.1);
+
+
+local function z()
+	integrate(0.1);
+	return zeta;
+end
+
+plot(z, 0, 300, 1);
+
+----------------------
 
 local function do_experiment()
 	local f = io.open("exp01/waving.txt", "w");
-	
 	for t=0, 100, 0.125 do
 		local offset, angle = waving:wave(t, 0);
 		local out	= string.format("%g %g %g", t, offset, angle);
 		f:write(out.."\n");
 	end
+	io.close(f);
 
+	local f = io.open("exp01/fr.txt", "w");
+	
+	for x=0, 20, 0.2 do
+		force = Fr(x);
+		local out	= string.format("%g %g", x, force);
+		f:write(out.."\n");
+	end
 	io.close(f);
 end
 
 do_experiment();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
