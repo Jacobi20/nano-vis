70833202a6e1376a3159dda6a974b453794b7c43
demiurghg <demiurghg@6d03c5d0-0fef-11df-8179-3f26cfb4ba5f>
2010-09-02 15:58:46 +0000

    Dev: exp01.lua

--- trunk/ship_sim/gamedata/exp01/exp01.lua
+++ trunk/ship_sim/gamedata/exp01/exp01.lua
@@ -6,43 +6,77 @@ print ("Experiment #1");
 
 --	world properties :
 local	gravity	=	9.8;				--	m/s^2
+local	g		=	gravity;
 local	gamma	=	gravity * 1000;		--	water weight density
 
+local function sqr(a) return a*a; end;
+
 -----------------------------------------------------------------------
---	submarine :
+--	SUBMARINE  --------------------------------------------------------
+-----------------------------------------------------------------------
+--	attributes :
 -----------------------------------------------------------------------
 
-local submarine = {
-	--	origin properties :
-	radius		=	5;		--	radius, m
-	length		=	100;    --	length, m
-	draft		=	7;		--	initial equlibrium keel depth
-	lambda33	=	0;		--	attached water mass
-	lambda44	=	0;		--	attached water inertia
-	mju33		=	0.8;	--	linear damping coeeficient
-	mju44		=	0.8;	--	angular damping coeeficient
-	
-	--	derived properties :
-	x	=	0;	--	linear offset
-	dx	=	0;	--	linear velocity
-	ddx	=	0;	--	linear acceleration
-	a	=	0;	--	angle (roll)
-	da	=	0;	--	angular velocity
-	dda	=	0;	--	angular acceleration
-	
-	--	temporary :
-	force	=	0;
-	torque	=	0;
-};                    
-
-
-local function calculate_submerged_volume()
-	local R = submarine.radius;
-	local T = submarine.draft;
-	local L = submarine.length;
-	local S = (T-R)*math.sqrt(T*(2*R-T)) + R*R*(math.asin((T-R)/R) + math.pi/2 );
+local function S()
+	return (T-R)*math.sqrt(T*(2*R-T)) + R*R*(math.asin((T-R)/R) + math.pi/2 )	--	(1.2)
 end
 
+local	R			=	5;		--	submarine's radius, m
+local	L			=	100;    --	submarine's length, m
+local	T			=	7;		--	submarine's initial draft - equlibrium keel depth
+local	Zg			=	-2;		--	center of mass vertical offset
+local	Zp			=	T - R;	--	distance between center and waterline
+local 	B0			=	math.sqrt(R*R - Zp*Zp);		--	initial waterline width 	(1.1)
+local	D			=	S() * L * gamma;			--	weight of the submarine		(1.2)
+local	M			=	D/g;						--	mass of the submarine		
+local	Ixx			=	M/12 * (B0*B0 + 4*Zg*Zg);	--	ineria						(1.8)
+	
+local 	lambda33	=	0;		--	attached water mass
+local 	lambda44	=	0;		--	attached water inertia
+local 	mu33		=	0.8;	--	linear damping coeeficient
+local 	mu44		=	0.8;	--	angular damping coeeficient
+	
+--	derived properties :
+local 	zeta		=	0;	--	linear offset
+local 	zeta_d		=	0;	--	linear velocity
+local 	zeta_dd		=	0;	--	linear acceleration
+local 	theta		=	0;	--	angle (roll)
+local 	theta_d		=	0;	--	angular velocity
+local 	theta_dd	=	0;	--	angular acceleration
+	
+local	M_l33		=	M   + lambda33;
+local	I_l44		=	Ixx + lambda44;
+
+-----------------------------------------------------------------------
+--	forces :
+-----------------------------------------------------------------------
+
+--	(1.9)	--
+local function Fd(zeta_d)
+	return mu33 * zeta_d;
+end
+
+--	(1.9)	--	
+local function Md(theta_d)
+	return mu44 * theta_d;
+end
+
+--	(1.10)	--
+local function Fr(zeta)
+	local A, B, C, D;
+	A 	= R*R * math.asin((zeta+Zp)/R);
+	B 	= R*R * math.asin(Zp/R);
+	C	= (zeta+Zp) * math.sqrt(R*R - sqr(Zp+zeta));
+	D	= Zp * math.sqrt(R*R - Zp*Zp);
+	return gamma * L * (A - B + C - D);
+end
+
+--	(1.12)	--	
+local function Mr(theta)
+	return D * (R-Zg) * math.sin(theta);
+end
+
+--local function
 
 local function update_forces(dt)
 	
