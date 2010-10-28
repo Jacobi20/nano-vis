926024027c12baec90ca1cfc7ff692f348028723
demiurghg <demiurghg@6d03c5d0-0fef-11df-8179-3f26cfb4ba5f>
2010-10-28 17:26:36 +0000

    Dev: HPC 2010 stuff (xmcd)

--- trunk/ship_sim/gamedata/config.lua
+++ trunk/ship_sim/gamedata/config.lua
@@ -4,8 +4,8 @@
 cfg.setvar('d3d_wireframe'                 , nil);
 cfg.setvar('d3d_fullscr'                   , false);
 cfg.setvar('d3d_mode'                      , 2);
-cfg.setvar('d3d_xpos'                      , 275);
-cfg.setvar('d3d_ypos'                      , 33);
+cfg.setvar('d3d_xpos'                      , 190);
+cfg.setvar('d3d_ypos'                      , 37);
 cfg.setvar('d3d_anisotropy_level'          , 16);
 cfg.setvar('d3d_mipmap_lod_bias'           , 0);
 cfg.setvar('ship_show_hull'                , true);
