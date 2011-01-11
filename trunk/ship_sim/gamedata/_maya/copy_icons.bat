253c1bf70375ea421813be91b9952d4fc938f8d9
demiurghg <demiurghg@6d03c5d0-0fef-11df-8179-3f26cfb4ba5f>
2011-01-11 10:35:39 +0000

    Delete all old stuff

--- trunk/ship_sim/gamedata/_maya/copy_icons.bat
+++
@@ -1,5 +0,0 @@
-@REM ------------------------------------------------------------------------------
-@REM	This file copies icons 'from mission/_maya/icons' to 'Maya pref/icons'
-@REM ------------------------------------------------------------------------------
-
-copy icons\*.bmp %USERPROFILE%\Documents\maya\2008\prefs\icons
