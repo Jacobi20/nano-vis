9377c8dfb6631ff596900d6c83f059466770cb8c
alazar.az@gmail.com <alazar.az@gmail.com@6d03c5d0-0fef-11df-8179-3f26cfb4ba5f>
2011-03-10 16:19:46 +0000

    Dev: camera animation

--- trunk/ship_sim/lua/control.lua
+++ trunk/ship_sim/lua/control.lua
@@ -93,7 +93,7 @@ input.bind ( "SPACE", 	"control.state.up = true", "control.state.up = false" );
 input.bind ( "C", 		"control.state.dn = true", "control.state.dn = false" );
 input.bind ( "P",		"control.state.iu = true", "control.state.iu = false" );
 input.bind ( "O",		"control.state.id = true", "control.state.id = false" );
-input.bind ( "L",		"game.playCameraAnim('scenes/camera_anim.eax', 30)", "");
+input.bind ( "L",		"game.playCameraAnim('scenes/uboat_xxi.eax', 30)", "");
 input.bind ( "K",		"game.stopCameraAnim()", "");
 
 local ANGULAR_VELOCITY	=	90;
