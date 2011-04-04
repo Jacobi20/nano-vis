2ca0004fc3d7259e254ec37257c98323d2e8d837
demiurghg <demiurghg@6d03c5d0-0fef-11df-8179-3f26cfb4ba5f>
2011-04-04 16:34:57 +0000

    Dev: some stuff

--- ship_sim/lua/cgame.lua
+++ ship_sim/lua/cgame.lua
@@ -120,6 +120,8 @@ input.bind ( "R",	 	"cgame.touch_ship_roll();	cgame.restart_log()", "" );
 input.bind ( "Q",	 	"cgame.restart_log()", "" );
 input.bind ( "W",	 	"cgame.setup_waving()", "" );
 
+input.bind ( "F2",	 	"cgame.setup_exp03()", "" );
+
 function touch_ship_roll()
 	local x,y,z,a,b,c = shipmodel.getPose(ship);
 	shipmodel.setPose(ship, x,y,z,a,0,15);
@@ -193,6 +195,63 @@ end
 
 -------------------------------------------------------------------------------
 
+local exp03_running	=	nil;
+local EXP03_TIME	=	800;
+
+function setup_exp03()
+
+	if not exp03_running then
+		exp03_running = 0
+	else
+		exp03_running = exp03_running + 1;
+	end
+	
+	local a	=	0;
+	local b = 	0;
+	local n	=	0;
+	
+	if exp03_running ==  0 then a = 2; b = 4; n = 256; end;
+	if exp03_running ==  1 then a = 3; b = 4; n = 256; end;
+	if exp03_running ==  2 then a = 4; b = 4; n = 256; end;
+	if exp03_running ==  3 then a = 5; b = 4; n = 256; end;
+	if exp03_running ==  4 then a = 6; b = 4; n = 256; end;
+	
+	if exp03_running ==  5 then a = 2; b = 4; n = 64; end;
+	if exp03_running ==  6 then a = 3; b = 4; n = 64; end;
+	if exp03_running ==  7 then a = 4; b = 4; n = 64; end;
+	if exp03_running ==  8 then a = 5; b = 4; n = 64; end;
+	if exp03_running ==  9 then a = 6; b = 4; n = 64; end;
+	
+	if exp03_running == 10 then a = 2; b = 4; n = 16; end;
+	if exp03_running == 11 then a = 3; b = 4; n = 16; end;
+	if exp03_running == 12 then a = 4; b = 4; n = 16; end;
+	if exp03_running == 13 then a = 5; b = 4; n = 16; end;
+	if exp03_running == 14 then a = 6; b = 4; n = 16; end;
+	
+	if exp03_running == 15 then a = 2; b = 4; n = 4; end;
+	if exp03_running == 16 then a = 3; b = 4; n = 4; end;
+	if exp03_running == 17 then a = 4; b = 4; n = 4; end;
+	if exp03_running == 18 then a = 5; b = 4; n = 4; end;
+	if exp03_running == 19 then a = 6; b = 4; n = 4; end;
+	
+	if exp03_running == 20 then
+		game.killEntity( ship );
+		ship		=	ships.createCutter(5, 0, -0.86, 90,0,0);
+		ship_log 	= 	nil;
+	end
+	
+	user.fr_waving_wind			=	0;
+	user.fr_waving_omega		=	2.3 * a / b;
+	user.fr_waving_narrowness	=	n;
+	game.setWaving( false );
+	game.setWaving( true );
+
+	game.killEntity( ship );
+	ship	=	ships.createCutter(5, 0, -0.86, 90,0,0);
+	ship_log = io.open(string.format("ship_pr_%02d_%02d_%03d.log", a,b, n), "w");
+end
+
+
 --
 --	frame()
 --
@@ -218,18 +277,26 @@ function frame( dtime )
 	
 	local sd = shipmodel.getDynamics(ship);
 	
+	
 	if sd.roll > 120  then shipwreck = true end
 	if sd.roll < -120 then shipwreck = true end
 
 	record_counter = record_counter + 1;
 	if record_counter > 5 then
-		if not shipwreck then
+		if not shipwreck and ship_log then
 			local out	= string.format("%f %f %f %f %f %f %f %f", sd.time, sd.yaw, sd.pitch, sd.roll, sd.position_x, sd.position_y, sd.position_z, sd.wave_height);
 			ship_log:write(out.."\n");
 			ship_log:flush();
 		end
 		record_counter = 0;
 	end
+
+	if exp03_running then
+		if sd.time > EXP03_TIME then
+			setup_exp03()
+		end
+	end
+	
 
 	core.debugString("Time ", sd.time);
 	
