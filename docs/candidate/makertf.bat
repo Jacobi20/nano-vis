3f79cd56d44b39fa311888fcc31002f39f27be41
demiurghg <demiurghg@6d03c5d0-0fef-11df-8179-3f26cfb4ba5f>
2011-05-25 17:01:16 +0000

    Dev: RR

---
+++ docs/candidate/makertf.bat
@@ -0,0 +1,9 @@
+mkdir 2doc
+mkdir 2doc\images
+copy *.tex 2doc\*.tex
+copy *.bib 2doc\*.bib
+copy *.bbl 2doc\*.bbl
+copy *.toc 2doc\*.toc
+copy images\*.png 2doc\images\*.png
+copy images\*.pdf 2doc\images\*.pdf
+
